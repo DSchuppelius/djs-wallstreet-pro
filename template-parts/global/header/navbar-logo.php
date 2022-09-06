b9e662c560a6fef659862b3b53d7493c9f9c7bc7
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2022-09-06 14:22:13 +0200

    Fixed Header

--- template-parts/global/header/navbar-logo.php
+++ template-parts/global/header/navbar-logo.php
@@ -7,25 +7,27 @@
  * License      : GNU General Public License v3 or later
  * License Uri  : http://www.gnu.org/licenses/gpl.html
  */
-$current_options = get_current_options();
+$current_options = get_current_options(); ?>
 
-if (has_custom_logo()) {
-    $custom_logo_id = get_theme_mod("custom_logo");
-    $image = wp_get_attachment_image_src($custom_logo_id, "full"); ?>
-    <a class="navbar-brand <?php echo display_header_text() ? "withText" : "noText"; ?>" href="<?php echo esc_url(home_url("/")); ?>">
-        <img src="<?php echo esc_url($image[0]); ?>" class=" img-responsive custom-logo">
-    </a>
-<?php }
-if (display_header_text()) { ?>
-    <div class="site-branding-text logo-link-url <?php echo has_custom_logo() ? "withLogo" : "noLogo"; ?>">
-        <h1 class="site-title">
-            <a class="navbar-brand" href="<?php echo esc_url(home_url("/")); ?>" rel="home">
-                <div class="wallstreet_title_head"><?php bloginfo("name"); ?></div>
-            </a>
-        </h1>
-        <?php $wallstreet_description = get_bloginfo("description", "display");
-        if ($wallstreet_description || is_customize_preview()) { ?>
-            <p class="site-description"> <?php echo $wallstreet_description; ?></p>
-        <?php } ?>
-    </div>
-<?php } ?>
+<div class="site-logo-area <?php echo has_custom_logo() ? "with-Logo" : "no-Logo"; ?> <?php echo display_header_text() ? "with-Text" : "no-Text"; ?>">
+    <?php if(has_custom_logo()) {
+        $custom_logo_id = get_theme_mod("custom_logo");
+        $image = wp_get_attachment_image_src($custom_logo_id, "full"); ?>
+        <a class="navbar-brand" href="<?php echo esc_url(home_url("/")); ?>">
+            <img src="<?php echo esc_url($image[0]); ?>" class=" img-responsive custom-logo">
+        </a>
+    <?php }
+    if(display_header_text()) { ?>
+        <div class="site-branding-text logo-link-url">
+            <h1 class="site-title">
+                <a class="navbar-brand" href="<?php echo esc_url(home_url("/")); ?>" rel="home">
+                    <div class="wallstreet_title_head"><?php bloginfo("name"); ?></div>
+                </a>
+            </h1>
+            <?php $wallstreet_description = get_bloginfo("description", "display");
+            if ($wallstreet_description || is_customize_preview()) { ?>
+                <p class="site-description"> <?php echo $wallstreet_description; ?></p>
+            <?php } ?>
+        </div>
+    <?php } ?>
+</div>
