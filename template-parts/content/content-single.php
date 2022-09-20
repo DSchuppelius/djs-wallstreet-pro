90129eeb21bc95f068e56977d951780211b04eb7
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2022-09-21 01:12:46 +0200

    fix excerpt code oversize

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -17,7 +17,7 @@ if (has_post_format("status")) {
         <h2><a href="<?php the_permalink(); ?>"><?php the_content_title(); ?></a></h2>
     </header>
     <section>
-        <?php the_content(); ?>
+        <div class="content single"><?php the_content(); ?></div>
     </section>
 <?php }
 get_named_template_parts("template-parts/content/content", ["meta-footer", "footer"]);
