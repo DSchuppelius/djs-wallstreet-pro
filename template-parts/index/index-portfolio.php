5ab2fdf4ae3c35fb8f89ca8371b654a463123f7b
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2023-01-06 15:28:42 +0100

    repair buton on diffrent domain

--- template-parts/index/index-portfolio.php
+++ template-parts/index/index-portfolio.php
@@ -80,9 +80,15 @@ if(defined("DJS_POSTTYPE_PLUGIN")) { ?>
                                                     <?php }
                                                     if (get_post_meta(get_the_ID(), "portfolio_project_button_text", true)) { ?>
                                                         <div class="portfolio-btn">
-                                                            <form action="<?php echo $meta_project_link; ?>" <?php blank_target(get_post_meta(get_the_ID(), "meta_button_target", true), 'method="get"'); ?>>
-                                                                <button class="btn small more portfolio" type="submit"><?php echo get_post_meta(get_the_ID(), "portfolio_project_button_text", true); ?></button>								
-                                                            </form>
+                                                            <?php if (str_contains($meta_project_link, get_site_url())) { ?>
+                                                                <form action="<?php echo $meta_project_link; ?>" <?php blank_target(get_post_meta(get_the_ID(), "meta_button_target", true), 'method="get"'); ?>>
+                                                                    <button class="btn small more portfolio" type="submit"><?php echo get_post_meta(get_the_ID(), "portfolio_project_button_text", true); ?></button>								
+                                                                </form>
+                                                            <?php } else { ?>
+                                                                <a class="button btn small more portfolio" <?php blank_target(get_post_meta(get_the_ID(), "meta_button_target", true)); ?> href="<?php echo $meta_project_link; ?>" >
+                                                                    <?php echo get_post_meta(get_the_ID(), "portfolio_project_button_text", true); ?>
+                                                                </a>
+                                                            <?php } ?>
                                                         </div>
                                                     <?php } ?>
                                                 </div>
